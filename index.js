8e51f89c396ef9be82831f776ede54ad43a3e052
Heromatic <117944235+TheHeromatic@users.noreply.github.com>
2024-03-03 19:50:11 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+
+function functionVolume(){
+    alert("now set volume")
+}
+function functionBrightness(){
+    alert("now set brightness")
+}
+
+function functionDarkMode(){
+    alert("now set dark mode")
+}
